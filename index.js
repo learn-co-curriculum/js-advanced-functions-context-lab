5eea1d0c94b31c0bfd8c4728ce4ab03e60ca53b3
Hpollard003 <hpollard003@gmail.com>
2021-04-25 10:51:42 -0400

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,47 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ const createEmployeeRecord = array => {
+    let testEmployee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+        func : () => this
 
-let allWagesFor = function () {
+};
+// return this.array
+return testEmployee
+};
+
+const createEmployeeRecords = arrArr => {
+    const catalog = arrArr.map(array => createEmployeeRecord(array))
+    return catalog
+}
+
+const createTimeInEvent = (employeeRecord) => {
+    const time = employeeRecord.split(' ')
+    let newEvent = {
+        type: 'TimeIn',
+        hour: parseInt(time[1]),
+        date: time[0]
+    }
+    // employeeRecord.timeInEvents.push(newEvent)
+    console.log(employeeRecord)
+}
+
+
+
+
+
+
+
+
+
+// Also confused 
+const allWagesFor = () => {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -19,4 +58,9 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+// finds employee by name re-used function from my intro_context_lab
+const findEmployeeByFirstName = (array, firstName) => {
+    firstName = array.find(x => x.firstName)
+    return firstName
 }
