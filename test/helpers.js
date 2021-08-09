ae5ce3264ccd30643715663b27b34d801492382d
musiccity365 <m_sackstein@yahoo.com>
2021-08-09 18:23:34 -0400

    commit tree acee7d626cfd466ee2476bf2a930986bcc56b0e1

--- test/helpers.js
+++ test/helpers.js
@@ -7,18 +7,20 @@ const path = require('path')
 const babel = require('babel-core');
 const spies = require('chai-spies')
 
-chai.use( spies );
+chai.use(spies);
 
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
 
 const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
+    path.resolve(__dirname, '..', 'index.js'), {
+        presets: ['env']
+    }
 );
 
 const src = babelResult.code
 
 jsdom({
-  html, src
+    html,
+    src,
+    url: "http://localhost"
 });
